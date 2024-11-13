c623e8a1fc7bcf1054216e40de9b30f095ad40e9
shokhova-eve <shokhovajane@gmail.com>
2024-11-13 11:44:22 +0100

    updated styles of auth

--- auth-handler.js
+++ auth-handler.js
@@ -1,8 +1,20 @@
 function openLoginForm() {
     const modal = document.getElementById('authModal');
+	const closeBtn = document.querySelector('.close');
     const loginForm = document.getElementById('loginForm');
     const signupForm = document.getElementById('signupForm');
     
+	// Close modal when clicking X
+	closeBtn.onclick = function() {
+		modal.style.display = 'none';
+	}
+
+	// Optional: Close modal when clicking outside of it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = 'none';
+		}
+	}
     loginForm.style.display = 'block';
     signupForm.style.display = 'none';
     modal.style.display = 'block';
